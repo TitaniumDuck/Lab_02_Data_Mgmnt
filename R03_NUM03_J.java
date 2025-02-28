510a9400f98dff45d697df8251098c2d05fe994b
TitaniumDuck <vanwagnerjustin@gmail.com>
2025-02-28 18:29:54 -0500

    Update R03_NUM03_J.java
    1. Added the Rule Number and Name as a comment
    2. Added the Subsection Number and Name as a comment

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,3 +1,6 @@
+// Rule 03 - Numeric Types and Operations (NUM)
+// Part NUM03-J - Use Integer Types that can Fully Represent the Possible Range of Unsigned Data
+
 publuc static int getInteger(DataInputStream is) throws IOException {
   return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
