e8aa6574fa0927fd85af0cabae40b05fdd501606
TitaniumDuck <vanwagnerjustin@gmail.com>
2025-02-28 21:58:23 -0500

    Create R10_THI00_J.java
    1. Added the Incorrect Code for this step

---
+++ R10_THI00_J.java
@@ -0,0 +1,13 @@
+// Rule 10 - Thread APIs (THI)
+// Part THI00-J - Do Not Invoke Thread.run()
+
+public final class Foo implements Runnable {
+  @Override public void run() {
+    // ...
+  }
+
+public static void main(String[] args) {
+  Foo foo = new Foo();
+  new Thread(foo).run();
+}
+}
