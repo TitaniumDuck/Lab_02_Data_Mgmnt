012f2694edc238eb8fb3ca268410b565483a7de1
TitaniumDuck <vanwagnerjustin@gmail.com>
2025-02-28 21:47:14 -0500

    Update R09_LCK01_J.java
    1. Updated the incorrect code with the correct code
    2. Correct code used the Integer solution for this step

--- R09_LCK01_J.java
+++ R09_LCK01_J.java
@@ -2,7 +2,7 @@
 // Part LCK01-J - Do Not Synchronize on Objects that may be Reused
 
 private int count = 0;
-private final Integer Lock = count; // Boxed primitive lock is shared
+private final Integer Lock = new Integer(count);
 
 public void doSomething() {
   synchronized (Lock) {
